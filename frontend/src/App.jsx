7d0444295ba5add0687cef26701f348d9b0cc844
your name <your-email@example.com>
2025-02-15 11:30:20 +0530

    create Route for multiple component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Outlet from './pages/Home.jsx'
-  import Header from './components/header.jsx';
-  import Footer from './components/Footer.jsx'
-
-function App() {
-  return (
-
- <>
-
-
- <Header/>
-<main>
-<Outlet /> 
-  </main>
-  <Footer/>
-
- </>
-  );
-}
-
-export default App;
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ForgotPassowrd from './pages/ForgotPassowrd.jsx';
+import SignUp from './pages/SignUp.jsx';
+  
+  function App() {
+    return (
+      <div>
+        
+        <Header/>
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassowrd />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+        <Footer/>
+      
+      </div>
+    );
+  }
+  
+  export default App;
