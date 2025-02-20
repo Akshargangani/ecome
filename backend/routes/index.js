209c1fba520dae6f3ba8bc448d70be9333283187
your name <your-email@example.com>
2025-02-20 23:28:53 +0530

    Add user authentication routes with signup, signin, and user details endpoints

---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const router = express.Router()
+
+const userSignUpController = require('../controller/user/userSignUp.jsx')
+const userSignInController = require('../controller/user/userSignIn.jsx')
+const userDetailsController = require('../controller/user/userDetails.jsx')
+const authToken = require('../middleware/authToken.jsx')
+
+router.post("/signup",userSignUpController)
+router.post("/signin",userSignInController)
+router.get("/user-details",authToken,userDetailsController)
+
+module.exports = router
